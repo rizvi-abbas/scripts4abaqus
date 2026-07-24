d8884fe02fd7a7ee07c221175f8a835e99cffde0
Rizvi, Abbas <abbas.rizvi@benhaschim.com>
2026-07-24 12:18:30 +0100

    Enhance data processing in MFile002.py
    Refactor data handling in MFile002.py to include data invariants and type. Update createFeild1 function to accept valid invariants.

--- src/scripts4abaqus/MFile002.py
+++ src/scripts4abaqus/MFile002.py
@@ -702,17 +702,17 @@ def add_result_to_odb1(odbFile, file_path, fields):
     #odb = openOdb(path=odbFile, readOnly=False)
     for field in fields:
         data = TensorsGet_data1(file_path, f"{field}.npy")
-        dataPosition = eval(metadata['dataPosition'])[field]
         field2 = field.split('_')
-        if len(data.shape) > 1:
-            if data.shape[1] == 6 and field2[1] == 'data':
-                dataType = TENSOR_3D_FULL
-                field = field2[0]
+        dataInvariants = eval(metadata['dataInvariants'])[field2[0]]
+        dataPosition = eval(metadata['dataPosition'])[field2[0]]
+        dataType = eval(metadata['dataType'])[field2[0]]
+        if field2[1] == 'data' and dataType == TENSOR_3D_FULL:
+            field = field2[0]
         else:
-            dataType = SCALAR
             data = tuple((v,) for v in data)
         labels = tuple(range(1,len(data)+1))
-        createFeild1(odb,'PART-1-1',labels,data,field,'',dataType,dataPosition)
+        createFeild1(odb,'PART-1-1',labels,data,field,'',dataType,dataPosition,validInvariants=dataInvariants)
+        
     odb.save()
     odb.close()
     
@@ -735,8 +735,8 @@ def createEqStrainRatio1(name, odbFile, subject, lesion):
     odb.save()
     odb.close()
 
-def createFeild1(odb,instance,labels,data,name,description,dataType,dataPosition):
-    tmpField = odb.steps['Step-1'].frames[-1].FieldOutput(name=name, description=description, type=dataType)
+def createFeild1(odb,instance,labels,data,name,description,dataType,dataPosition, validInvariants=()):
+    tmpField = odb.steps['Step-1'].frames[-1].FieldOutput(name=name, description=description, type=dataType, validInvariants=validInvariants)
     tmpField.addData(position=dataPosition, instance=odb.rootAssembly.instances[instance], labels=labels, data=data)
 
 
@@ -821,11 +821,15 @@ def writeFieldTensors1(job,output_file,fields,metadata={},apply_func_all={}):
     import numpy as np
     file_path = Path(output_file)
     file_path.unlink(missing_ok=True)
+    dataInvariants = {}
     dataPosition = {}
+    dataType = {}
     odb = session.openOdb(name=job)
     if 'SectionName' in fields:
         fields.remove('SectionName')
         #append_data(file_path,'SectionName',df[['Section Name']],unique_string=True)
+
+    
 
     
     for field in fields:
@@ -835,14 +839,18 @@ def writeFieldTensors1(job,output_file,fields,metadata={},apply_func_all={}):
             data = np.array([getattr(v,field2[1]) for v in odb.steps['Step-1'].frames[-1].fieldOutputs[field2[0]].values])
             if apply_func is not None: data = apply_func(data,*args)
             TensorsAppend_data1(file_path, field, data )
-            dataPosition[field] = odb.steps['Step-1'].frames[-1].fieldOutputs[field2[0]].values[0].position
+            dataInvariants[field2[0]] = odb.steps['Step-1'].frames[-1].fieldOutputs[field2[0]].validInvariants
+            dataPosition[field2[0]] = odb.steps['Step-1'].frames[-1].fieldOutputs[field2[0]].locations[0].position
+            dataType[field2[0]] = odb.steps['Step-1'].frames[-1].fieldOutputs[field2[0]].type
 
     for coord in list(set(dataPosition.values())):
         TensorsAppend_data1(file_path,f"COORD_{coord}",np.array([getattr(v,'data') for v in odb.steps['Step-1'].frames[-1].fieldOutputs['COORD'].getSubset(position=coord).values]) )
     #TensorsAppend_data1(file_path,'COORD_NODAL',np.array([getattr(v,'data') for v in odb.steps['Step-1'].frames[-1].fieldOutputs['COORD'].getSubset(position=NODAL).values]) )
     #TensorsAppend_data1(file_path,'COORD_INTEGRATION_POINT',np.array([getattr(v,'data') for v in odb.steps['Step-1'].frames[-1].fieldOutputs['COORD'].getSubset(position=INTEGRATION_POINT).values]) )
     
+    metadata['dataType'] = str(dataType)
     metadata['dataPosition'] = str(dataPosition)
+    metadata['dataInvariants'] = str(dataInvariants)
     TensorsAppend_metadata1(file_path, metadata)
     odb.close()
     
